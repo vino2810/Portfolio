f69a1701029b1bfe40ec08428ad701b86ab396b7
vino2810 <vinothkumar10073@gmail.com>
2024-08-27 21:40:02 +0530

    final update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,16 @@
 import HeroImg from '../Assets/bgrem.png';
 import { BsLinkedin } from "react-icons/bs";
-import { FaFacebookSquare } from "react-icons/fa";
+import { FaWhatsappSquare } from "react-icons/fa";
 import { AiOutlineInstagram } from "react-icons/ai";
 import { AiOutlineGithub } from "react-icons/ai";
 
 export default function Hero(){
 
     const config ={
-        subtitle : "I am a fullstack Developer",
+        subtitle : "Fullstack Developer",
         social : {
               Linkedin: 'https://www.linkedin.com/in/vinothkumar2810/',
-              faceBook: '',
+              whatsapp: 'https://wa.link/4ktg0f',
                gitHub: 'https://github.com/vino2810',
         }
     }
@@ -21,7 +21,7 @@ export default function Hero(){
         <div className='flex py-7'>
             <a href={config.social.Linkedin} className='pr-3  text-[#0A66C2] hover:text-white '> <BsLinkedin  size={30}/></a>
             <a href={config.social.gitHub} className='pr-3 hover:text-white text-gray-600 md:text-[#181717] '> <AiOutlineGithub  size={30}/></a>
-            <a href={config.social} className='pr-3  hover:text-white text-[#0A66C2]' > <FaFacebookSquare size={30}/></a>
+            <a href={config.social.whatsapp} className='pr-3  hover:text-white text-[#4cd63a]' > <FaWhatsappSquare size={30}/></a>
             <a href={config.social} className='pr-3  hover:text-white text-[#E4405F]'> <AiOutlineInstagram size={30}/></a>
         </div>
         </div>
--- src/components/about.js
+++ src/components/about.js
@@ -1,17 +1,21 @@
-import aboutimg from '../Assets/aboutimg.jpeg'
+
 
 export default function About() {
     const config = {
-        line1: '"Im a  BSc Computer Science graduate with a strong foundation in Full Stack Development. My skill set includes Java, JavaScript, HTML, CSS, React JS, and MySQL, enabling me to build dynamic, user-friendly web applications from front-end to back-end."',
-
+        line1: '"Hello! Im Vinothkumar, a 2023 BSc Computer Science graduate with a passion for technology and software development."',
+        line2: 'My journey in technology has equipped me with a diverse skill set in both front-end and back-end development, including languages and frameworks like Java, JavaScript, React JS, HTML, CSS, and MySQL.',
+        line3: 'As a lifelong learner👨🏻‍💻, I’m driven by the excitement of new challenges and the opportunity to innovate within the tech space.'
     }
 
     return <section className='flex flex-col md:flex-row justify-center  px-5' id='about'>
-        <img src={aboutimg} alt='resume' className='md:w-1/3 mx-3 rounded-full bg-transparent' data-aos-duration="1000" data-aos="zoom-out-right" />
-        <div className='md:w-1/2 flex justify-center '>
+
+        {/* <img src={aboutimg} alt='resume' className='md:w-1/3 mx-3 rounded-full bg-transparent' data-aos-duration="1000" data-aos="zoom-out-right"/> */}
+        <div className=' flex justify-center '>
             <div className='flex flex-col justify-center text-white'>
                 <h1 className='text-4xl text-white border-b-2 mb-3 w-[160px] font-google-font '>About me</h1>
-                <p className='text-white pb-4 font-google-font text-2xl ' data-aos-duration="1000" data-aos="flip-right"> {config.line1}</p>
+                <p className=' pb-4 font-sans text-lg text-white  ' data-aos-duration="1000" data-aos="flip-right"> {config.line1}</p>
+                <p className='text-white  pb-4 font-sans text-lg ' data-aos-duration="1000" data-aos="flip-left"> {config.line2}</p>
+                <p className='text-white  pb-4 font-sans text-lg ' data-aos-duration="1000" data-aos="fade-up"> {config.line3}</p>
 
             </div>
 
