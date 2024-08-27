9740f5c62727cbce8422d05985900597a0de9f22
vino2810 <vinothkumar10073@gmail.com>
2024-08-27 21:52:26 +0530

    update

--- src/components/about.js
+++ src/components/about.js
@@ -8,8 +8,6 @@ export default function About() {
     }
 
     return <section className='flex flex-col md:flex-row justify-center  px-5' id='about'>
-
-        {/* <img src={aboutimg} alt='resume' className='md:w-1/3 mx-3 rounded-full bg-transparent' data-aos-duration="1000" data-aos="zoom-out-right"/> */}
         <div className=' flex justify-center '>
             <div className='flex flex-col justify-center text-white'>
                 <h1 className='text-4xl text-white border-b-2 mb-3 w-[160px] font-google-font '>About me</h1>
